41514843e5060fbe60a359c06c3a123f907083cb
My Coding Flair <109297209+mycodingflair@users.noreply.github.com>
2022-09-03 16:24:32 +0530

    Update Datatype.cpp
    Data types, Typecasting and operators

--- Day 1 - 10/Datatype.cpp
+++ Day 1 - 10/Datatype.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int a = 123;
+/*    int a = 123;
     cout<<a<<endl;
 
     char b = 'v';
@@ -19,5 +19,60 @@ int main(){
 
     int size = sizeof(d);
     cout<<"Size of a is :"<<size<<endl;
+
+
+    int a = 'a';
+    cout<<a<<endl;
+ 
+    char ch = 98;
+    cout<< ch<< endl;
+
+    char ch1 = 123456;
+    cout <<ch1<<endl;
+*/
+
+/* Important : msb is 1, since unsigned considers only positive numbers. it will consider -112 as a huge number since -ve it will take MSB as 1
+
+    unsigned int a =-112;
+    cout <<a<<endl;
+
+/* Operators
+float/int —> float
+double/int —> double 
+
+
+    int a = 2.0/5;//since it is stored in int it will be 0 not 0.0
+    cout<<a<<endl;
+    cout<<2.0/5<<endl;
+
+/*relational operator 
+
+    int a = 2;
+    int b = 3;
+
+    bool first = (a==b);
+    cout<<first<<endl;
+
+    bool second = (a>b);
+    cout<<second<<endl;
+
+    bool third = (a<b);
+    cout<<third<<endl;
+
+    bool fourth = (a>=b);
+    cout<<fourth<<endl;
+
+    bool fifth = (a<=b);
+    cout<<fifth<<endl;
+
+    bool sixth = (a!=b);
+    cout<<sixth<<endl;
+
+logical operator
+*/
+    int a = 21;
+    cout<< !a<<endl;
+    int b = 0;
+    cout<<!b<<endl;
 
 }
